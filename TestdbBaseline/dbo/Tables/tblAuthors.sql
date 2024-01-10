9871df8cbaea9db83edb1275b9116f5dae4967d7
Pablo-Centeno <pablocenteno321@gmail.com>
2024-01-10 23:10:17 +1300

    Baseline

---
+++ TestdbBaseline/dbo/Tables/tblAuthors.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[tblAuthors] (
+    [Id]          INT           IDENTITY (1, 1) NOT NULL,
+    [Author_name] NVARCHAR (50) NULL,
+    [country]     NVARCHAR (50) NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
+
+GO
+ALTER TABLE [dbo].[tblAuthors] ENABLE CHANGE_TRACKING WITH (TRACK_COLUMNS_UPDATED = ON);
+
---
+++ TestdbBaseline/dbo/Tables/tblBooks.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[tblBooks] (
+    [Id]         INT IDENTITY (1, 1) NOT NULL,
+    [Auhthor_id] INT NULL,
+    [Price]      INT NULL,
+    [Edition]    INT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC),
+    FOREIGN KEY ([Auhthor_id]) REFERENCES [dbo].[tblAuthors] ([Id])
+);
+
+
+GO
+ALTER TABLE [dbo].[tblBooks] ENABLE CHANGE_TRACKING WITH (TRACK_COLUMNS_UPDATED = ON);
+
